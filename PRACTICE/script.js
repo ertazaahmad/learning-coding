ddff95628b56e8faa9df4bf3840f0d94a05135b8
Mohd Ertaza Ahmad <arshertaza@gmail.com>
2025-07-01 22:53:48 +0530

    practice of 1st july

---
+++ PRACTICE/script.js
@@ -0,0 +1,7 @@
+let num = 25;
+
+if (num % 2 === 0 && num % 3 === 0) {
+    console.log("The number is divisible by both 2 and 3.");
+}
+
+else  { console.log(num + " is not divisible by both 2 and 3."); }
