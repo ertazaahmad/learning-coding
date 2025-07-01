8ca1f4c11b7ac4517adeb2a9badc55956c5d35b0
Mohd Ertaza Ahmad <arshertaza@gmail.com>
2025-07-01 23:02:24 +0530

    number divisiblity checker

--- PRACTICE/script.js
+++ PRACTICE/script.js
@@ -1,7 +1,17 @@
-let num = 25;
+let num = 77;
 
 if (num % 2 === 0 && num % 3 === 0) {
-    console.log("The number is divisible by both 2 and 3.");
+    console.log(num + " is divisible by 2 and 3 .");
+
 }
 
-else  { console.log(num + " is not divisible by both 2 and 3."); }
+else if (num % 2 === 0 ) {
+    console.log(num + " is divisible by 2 .");
+}  
+
+else if ( num % 3 === 0) {
+    console.log(num + " is divisible by 3 .");
+
+}
+
+else  { console.log(num + " is not divisible by  2 or 3."); }
