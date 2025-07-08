715f743e115c6a187f167e971d381bae4cd884a4
Mohd Ertaza Ahmad <arshertaza@gmail.com>
2025-07-08 11:32:27 +0530

    practice

---
+++ practice/script.js
@@ -0,0 +1,7 @@
+// let boxes = document.getElementsByClassName("box")
+
+// boxes[2].style.backgroundColor = "pink"
+// boxes[3].style.backgroundColor = "purple"
+// console.log(boxes)
+
+document.getElementById("tbox").style.backgroundColor = "red"
